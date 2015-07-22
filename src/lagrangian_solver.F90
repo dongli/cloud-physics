c2908827da216fe15c660aa12464ba9def401544
Li Dong <dongli@lasg.iap.ac.cn>
2015-07-22 09:58:23 +0800

    Small changes
    Fix some small errors.
    
    Rename some variables.
    
    Add two more text lines on the plot.

--- src/lagrangian_solver.F90
+++ src/lagrangian_solver.F90
@@ -30,10 +30,10 @@ contains
 
     subroutine lagrangian_solver_init(num_bin_, r, dr, f)
 
-        integer, intent(in) :: num_bin_ ! Initial discretized bin number.
-        real(8), intent(in) :: r(num_bin) ! Initial droplet radius left side [cm].
-        real(8), intent(in) :: dr(num_bin) ! Initial bin interval [cm].
-        real(8), intent(in) :: f(num_bin) ! Initial drop-size distribution [per unit air mass?].
+        integer, intent(in) :: num_bin_       ! Initial discretized bin number.
+        real(8), intent(in) :: r(num_bin_)    ! Initial droplet radius at left side [cm].
+        real(8), intent(in) :: dr(num_bin_-1) ! Initial bin interval [cm].
+        real(8), intent(in) :: f(num_bin_)    ! Initial drop-size distribution [per unit air mass?].
 
         integer i
 
@@ -42,7 +42,7 @@ contains
         num_bin = 0
         do i = 1, num_bin_-1
             if (f(i) > 0) then
-                bins(i)%r = r(i)+dr(i)*0.5
+                bins(i)%r = r(i)+dr(i)*0.5d0
                 bins(i)%dr = dr(i)
                 bins(i)%f = f(i)
                 num_bin = num_bin+1
@@ -72,13 +72,9 @@ contains
             end subroutine growth_rate
         end interface
 
-        real(8) drdt1, k1
-        real(8) drdt2, k2
-        real(8) drdt3, k3
-        real(8) drdt4, k4
-        real(8) r0, dr0
-        real(8) r1, dr1
-        real(8) div_drdt
+        real(8) v1, v2, v3, v4, r0, dr0
+        real(8) k1, k2, k3, k4, r1, dr1
+        real(8) div
         real(8) total_droplet_num
         integer i
 
@@ -88,27 +84,19 @@ contains
             dr0 = bins(i)%dr
             ! Update central droplet radius r.
             ! Update bin interval dr.
-            call growth_rate(T, p, es, S, r0, drdt1, div_drdt)
-            r1 = r0+drdt1*dt*0.5d0
-            k1 = dr0*div_drdt
-            dr1 = dr0+k1*dt*0.5d0
-            call growth_rate(T, p, es, S, r0, drdt2, div_drdt)
-            r1 = r0+drdt2*dt*0.05d0
-            k2 = dr1*div_drdt
-            dr1 = dr0+k2*dt*0.5d0
-            call growth_rate(T, p, es, S, r0, drdt3, div_drdt)
-            r1 = r0+drdt3*dt
-            k3 = dr1*div_drdt
-            dr1 = dr0+k3*dt
-            call growth_rate(T, p, es, S, r0, drdt4, div_drdt)
-            bins(i)%r = r0+(drdt1+2.0d0*drdt2+2.0d0*drdt3+drdt4)/6.0d0*dt
-            k4 = dr1*div_drdt
+            call growth_rate(T, p, es, S, r0, v1, div); k1 = dr0*div
+            r1 = r0+v1*dt*0.5d0; dr1 = dr0+k1*dt*0.5d0
+            call growth_rate(T, p, es, S, r0, v2, div); k2 = dr1*div
+            r1 = r0+v2*dt*0.5d0; dr1 = dr0+k2*dt*0.5d0
+            call growth_rate(T, p, es, S, r0, v3, div); k3 = dr1*div
+            r1 = r0+v3*dt;       dr1 = dr0+k3*dt
+            call growth_rate(T, p, es, S, r0, v4, div); k4 = dr1*div
+            bins(i)%r = r0+(v1+2.0d0*v2+2.0d0*v3+v4)/6.0d0*dt
             bins(i)%dr = dr0+(k1+2.0d0*k2+2.0d0*k3+k4)/6.0d0*dt
             bins(i)%f = bins(i)%f*dr0/bins(i)%dr
             total_droplet_num = total_droplet_num+bins(i)%dr*bins(i)%f
         end do
         print "(F30.15)", total_droplet_num
-        print *, bins(1)%r, bins(2)%r
 
     end subroutine lagrangian_solver_run
 
